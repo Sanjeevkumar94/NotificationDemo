3c29bf9087dec3f0befa40bee8177b690048cb40
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2020-07-12 18:21:28 +0530

    generate token

--- app/src/main/java/com/example/notificationdemo/MainActivity.java
+++ app/src/main/java/com/example/notificationdemo/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.notificationdemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +20,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tvScore = findViewById(R.id.tv_score);
-        if(getIntent()!=null && getIntent().hasExtra("score")){
+       /* if(getIntent()!=null && getIntent().hasExtra("score")){
             for(String key : getIntent().getExtras().keySet()){
                 tvScore.append(getIntent().getExtras().getString(key)+"\n");
             }
-        }
+        }*/
+
+
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if(task.isSuccessful()){
+                            String token = task.getResult().getToken();
+                            tvScore.setText(token);
+                            Log.d("tokenGenerated",token);
+                        }
+                    }
+                });
+
     }
 }
