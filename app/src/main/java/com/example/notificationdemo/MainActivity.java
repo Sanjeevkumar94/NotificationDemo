97a091b15ccb6ff6f95cf7660a089dd92a1468e9
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2020-07-12 17:19:36 +0530

    show data from notification

--- app/src/main/java/com/example/notificationdemo/MainActivity.java
+++ app/src/main/java/com/example/notificationdemo/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.notificationdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvScore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvScore = findViewById(R.id.tv_score);
+        if(getIntent()!=null && getIntent().hasExtra("score")){
+            for(String key : getIntent().getExtras().keySet()){
+                tvScore.append(getIntent().getExtras().getString(key)+"\n");
+            }
+        }
     }
 }
